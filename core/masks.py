776b5e166c3af39d0e41ce6c5b9be9e74c201661
Adam Cohen Hillel <ahillel@cadosecurity.com>
2023-03-22 14:34:50 +0000

    statics

--- core/masks.py
+++ core/masks.py
@@ -2,7 +2,7 @@
 """
 import re
 from abc import ABC, abstractmethod
-from typing import Dict, Tuple
+from typing import Any, List
 
 import nltk
 
@@ -18,7 +18,7 @@ class MaskBase(ABC):
 
     @staticmethod
     @abstractmethod
-    def find(data: str) -> Tuple[str, Dict[str, str]]:
+    def find(data: str) -> List[Any]:
         """Implement this method
 
         :param data: Data to mask
@@ -32,7 +32,7 @@ class IPMask(MaskBase):
     """IP addresses
     """
     @staticmethod
-    def find(data: str) -> Tuple[str, Dict[str, str]]:
+    def find(data: str) -> List[Any]:
         return re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b", data)
 
 
@@ -40,7 +40,7 @@ class NamesMask(MaskBase):
     """Persons names
     """
     @staticmethod
-    def find(data: str) -> Tuple[str, Dict[str, str]]:
+    def find(data: str) -> List[Any]:
         sentt = nltk.ne_chunk(nltk.pos_tag(nltk.tokenize.word_tokenize(data)), binary=False)
         person_list = []
         person = []
@@ -61,7 +61,7 @@ class LinkMask(MaskBase):
     """Web links
     """
     @staticmethod
-    def find(data: str) -> Tuple[str, Dict[str, str]]:
+    def find(data: str) -> List[Any]:
         return re.findall(r'(?i)((?:https?://|www\d{0,3}[.])?[a-z0-9.\-]+[.](?:(?:international)|(?:construction)|(?:contractors)|(?:enterprises)|(?:photography)|(?:immobilien)|(?:management)|(?:technology)|(?:directory)|(?:education)|(?:equipment)|(?:institute)|(?:marketing)|(?:solutions)|(?:builders)|(?:clothing)|(?:computer)|(?:democrat)|(?:diamonds)|(?:graphics)|(?:holdings)|(?:lighting)|(?:plumbing)|(?:training)|(?:ventures)|(?:academy)|(?:careers)|(?:company)|(?:domains)|(?:florist)|(?:gallery)|(?:guitars)|(?:holiday)|(?:kitchen)|(?:recipes)|(?:shiksha)|(?:singles)|(?:support)|(?:systems)|(?:agency)|(?:berlin)|(?:camera)|(?:center)|(?:coffee)|(?:estate)|(?:kaufen)|(?:luxury)|(?:monash)|(?:museum)|(?:photos)|(?:repair)|(?:social)|(?:tattoo)|(?:travel)|(?:viajes)|(?:voyage)|(?:build)|(?:cheap)|(?:codes)|(?:dance)|(?:email)|(?:glass)|(?:house)|(?:ninja)|(?:photo)|(?:shoes)|(?:solar)|(?:today)|(?:aero)|(?:arpa)|(?:asia)|(?:bike)|(?:buzz)|(?:camp)|(?:club)|(?:coop)|(?:farm)|(?:gift)|(?:guru)|(?:info)|(?:jobs)|(?:kiwi)|(?:land)|(?:limo)|(?:link)|(?:menu)|(?:mobi)|(?:moda)|(?:name)|(?:pics)|(?:pink)|(?:post)|(?:rich)|(?:ruhr)|(?:sexy)|(?:tips)|(?:wang)|(?:wien)|(?:zone)|(?:biz)|(?:cab)|(?:cat)|(?:ceo)|(?:com)|(?:edu)|(?:gov)|(?:int)|(?:mil)|(?:net)|(?:onl)|(?:org)|(?:pro)|(?:red)|(?:tel)|(?:uno)|(?:xxx)|(?:ac)|(?:ad)|(?:ae)|(?:af)|(?:ag)|(?:ai)|(?:al)|(?:am)|(?:an)|(?:ao)|(?:aq)|(?:ar)|(?:as)|(?:at)|(?:au)|(?:aw)|(?:ax)|(?:az)|(?:ba)|(?:bb)|(?:bd)|(?:be)|(?:bf)|(?:bg)|(?:bh)|(?:bi)|(?:bj)|(?:bm)|(?:bn)|(?:bo)|(?:br)|(?:bs)|(?:bt)|(?:bv)|(?:bw)|(?:by)|(?:bz)|(?:ca)|(?:cc)|(?:cd)|(?:cf)|(?:cg)|(?:ch)|(?:ci)|(?:ck)|(?:cl)|(?:cm)|(?:cn)|(?:co)|(?:cr)|(?:cu)|(?:cv)|(?:cw)|(?:cx)|(?:cy)|(?:cz)|(?:de)|(?:dj)|(?:dk)|(?:dm)|(?:do)|(?:dz)|(?:ec)|(?:ee)|(?:eg)|(?:er)|(?:es)|(?:et)|(?:eu)|(?:fi)|(?:fj)|(?:fk)|(?:fm)|(?:fo)|(?:fr)|(?:ga)|(?:gb)|(?:gd)|(?:ge)|(?:gf)|(?:gg)|(?:gh)|(?:gi)|(?:gl)|(?:gm)|(?:gn)|(?:gp)|(?:gq)|(?:gr)|(?:gs)|(?:gt)|(?:gu)|(?:gw)|(?:gy)|(?:hk)|(?:hm)|(?:hn)|(?:hr)|(?:ht)|(?:hu)|(?:id)|(?:ie)|(?:il)|(?:im)|(?:in)|(?:io)|(?:iq)|(?:ir)|(?:is)|(?:it)|(?:je)|(?:jm)|(?:jo)|(?:jp)|(?:ke)|(?:kg)|(?:kh)|(?:ki)|(?:km)|(?:kn)|(?:kp)|(?:kr)|(?:kw)|(?:ky)|(?:kz)|(?:la)|(?:lb)|(?:lc)|(?:li)|(?:lk)|(?:lr)|(?:ls)|(?:lt)|(?:lu)|(?:lv)|(?:ly)|(?:ma)|(?:mc)|(?:md)|(?:me)|(?:mg)|(?:mh)|(?:mk)|(?:ml)|(?:mm)|(?:mn)|(?:mo)|(?:mp)|(?:mq)|(?:mr)|(?:ms)|(?:mt)|(?:mu)|(?:mv)|(?:mw)|(?:mx)|(?:my)|(?:mz)|(?:na)|(?:nc)|(?:ne)|(?:nf)|(?:ng)|(?:ni)|(?:nl)|(?:no)|(?:np)|(?:nr)|(?:nu)|(?:nz)|(?:om)|(?:pa)|(?:pe)|(?:pf)|(?:pg)|(?:ph)|(?:pk)|(?:pl)|(?:pm)|(?:pn)|(?:pr)|(?:ps)|(?:pt)|(?:pw)|(?:py)|(?:qa)|(?:re)|(?:ro)|(?:rs)|(?:ru)|(?:rw)|(?:sa)|(?:sb)|(?:sc)|(?:sd)|(?:se)|(?:sg)|(?:sh)|(?:si)|(?:sj)|(?:sk)|(?:sl)|(?:sm)|(?:sn)|(?:so)|(?:sr)|(?:st)|(?:su)|(?:sv)|(?:sx)|(?:sy)|(?:sz)|(?:tc)|(?:td)|(?:tf)|(?:tg)|(?:th)|(?:tj)|(?:tk)|(?:tl)|(?:tm)|(?:tn)|(?:to)|(?:tp)|(?:tr)|(?:tt)|(?:tv)|(?:tw)|(?:tz)|(?:ua)|(?:ug)|(?:uk)|(?:us)|(?:uy)|(?:uz)|(?:va)|(?:vc)|(?:ve)|(?:vg)|(?:vi)|(?:vn)|(?:vu)|(?:wf)|(?:ws)|(?:ye)|(?:yt)|(?:za)|(?:zm)|(?:zw))(?:/[^\s()<>]+[^\s`!()\[\]{};:\'".,<>?\xab\xbb\u201c\u201d\u2018\u2019])?)', data)
 
 
@@ -69,7 +69,7 @@ class PhoneMask(MaskBase):
     """Phone numbers
     """
     @staticmethod
-    def find(data: str) -> Tuple[str, Dict[str, str]]:
+    def find(data: str) -> List[Any]:
         return re.findall(r'''((?:(?<![\d-])(?:\+?\d{1,3}[-.\s*]?)?(?:\(?\d{3}\)?[-.\s*]?)?\d{3}[-.\s*]?\d{4}(?![\d-]))|(?:(?<![\d-])(?:(?:\(\+?\d{2}\))|(?:\+?\d{2}))\s*\d{2}\s*\d{3}\s*\d{4}(?![\d-])))''', data)
 
 
@@ -77,7 +77,7 @@ class EmailMask(MaskBase):
     """Email addresses
     """
     @staticmethod
-    def find(data: str) -> Tuple[str, Dict[str, str]]:
+    def find(data: str) -> List[Any]:
         return re.findall(r"([a-z0-9!#$%&'*+\/=?^_`{|.}~-]+@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)", data)
 
 
@@ -85,5 +85,5 @@ class CreditCardMask(MaskBase):
     """Credit Card
     """
     @staticmethod
-    def find(data: str) -> Tuple[str, Dict[str, str]]:
+    def find(data: str) -> List[Any]:
         return re.findall(r'\b(?:\d{4}-){3}\d{4}|\b\d{16}\b', data)
--- tests/test_core/test_masks.py
+++ tests/test_core/test_masks.py
@@ -16,102 +16,94 @@ class MasksTests(unittest.TestCase):
     """
     """
 
-    @classmethod
-    def setUpClass(cls) -> None:
-        pass
-
-    @classmethod
-    def tearDownClass(cls):
-        pass
-
-    def test_local_ip_mask(self):
+    def test_local_ip_mask(self) -> None:
         data = "This is a string with IP address 127.0.0.1"
         found = IPMask.find(data)
         self.assertEqual(len(found), 1)
         self.assertEqual(found[0], "127.0.0.1")
 
-    def test_non_local_ip_mask(self):
+    def test_non_local_ip_mask(self) -> None:
         data = "This is a string with non local 172.217.22.14 IP address"
         found = IPMask.find(data)
         self.assertEqual(len(found), 1)
         self.assertEqual(found[0], "172.217.22.14")
 
-    def test_a_few_local_ip_addresses(self):
+    def test_a_few_local_ip_addresses(self) -> None:
         data = "This is a string with a few 172.217.22.14 IP address 127.0.0.1"
         found = IPMask.find(data)
         self.assertEqual(len(found), 2)
         self.assertTrue("172.217.22.14" in found)
         self.assertTrue("127.0.0.1" in found)
 
-    def test_name_without_lastname(self):
+    def test_name_without_lastname(self) -> None:
         data = "Adam is going to do some cool shit in this world"
         found = NamesMask.find(data)
         self.assertEqual(len(found), 1)
         self.assertTrue("Adam" in found)
 
-    def test_name_with_lastname(self):
+    def test_name_with_lastname(self) -> None:
         data = "Adam Cohen is going to do some cool shit in this world"
         found = NamesMask.find(data)
         self.assertEqual(len(found), 2)
         self.assertTrue("Adam" in found)
         self.assertTrue("Cohen" in found)
 
-    def test_name_with_long_lastname(self):
+    def test_name_with_long_lastname(self) -> None:
         data = "Adam Cohen Hillel is going to do some cool shit in this world"
         found = NamesMask.find(data)
         self.assertEqual(len(found), 2)
         self.assertTrue("Adam" in found)
         self.assertTrue("Cohen Hillel" in found)
 
-    def test_short_link(self):
+    def test_short_link(self) -> None:
         data = "The user clicked on google.com"
         found = LinkMask.find(data)
         self.assertEqual(len(found), 1)
         self.assertTrue("google.com" in found)
 
-    def test_www_link(self):
+    def test_www_link(self) -> None:
         data = "The user clicked on www.google.com"
         found = LinkMask.find(data)
         self.assertEqual(len(found), 1)
         self.assertTrue("www.google.com" in found)
 
-    def test_http_link(self):
+    def test_http_link(self) -> None:
         data = "The user clicked on https://www.google.com"
         found = LinkMask.find(data)
         self.assertEqual(len(found), 1)
         self.assertTrue("https://www.google.com" in found)
 
-    def test_uk_international_phone_number(self):
+    def test_uk_international_phone_number(self) -> None:
         data = "The user called +447831879184"
         found = PhoneMask.find(data)
         self.assertEqual(len(found), 1)
         self.assertTrue("+447831879184" in found)
 
-    def test_us_international_phone_number(self):
+    def test_us_international_phone_number(self) -> None:
         data = "The user called +12025550196"
         found = PhoneMask.find(data)
         self.assertEqual(len(found), 1)
         self.assertTrue("+12025550196" in found)
 
-    def test_no_international_phone_number(self):
+    def test_no_international_phone_number(self) -> None:
         data = "The user called 07831879184"
         found = PhoneMask.find(data)
         self.assertEqual(len(found), 1)
         self.assertTrue("07831879184" in found)
 
-    def test_email_gmail(self):
+    def test_email_gmail(self) -> None:
         data = "testing with thisisatest@gmail.com"
         found = EmailMask.find(data)
         self.assertEqual(len(found), 1)
         self.assertTrue("thisisatest@gmail.com" in found)
 
-    def test_not_known_mail_address(self):
+    def test_not_known_mail_address(self) -> None:
         data = "testing with thisisatest@company.com"
         found = EmailMask.find(data)
         self.assertEqual(len(found), 1)
         self.assertTrue("thisisatest@company.com" in found)
 
-    def test_credit_card(self):
+    def test_credit_card(self) -> None:
         """From `https://www.paypalobjects.com/en_AU/vhelp/paypalmanager_help/credit_card_numbers.htm`
         """
         data = "testing with 4012-8888-8882-1881"
