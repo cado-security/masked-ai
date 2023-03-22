1de8011b7da04b0cf4c3bacef5d18ccb48b33ef6
chriscado <cdoman@cadosecurity.com>
2023-03-22 10:23:01 +0000

    Add pip

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+VERSION = '1.0.0'
+
+setup(
+    name='masked-ai',
+    version=VERSION,
+    description='Masked AI',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Cado Security',
+    author_email='maskedai@cadosecurity.com',
+    url='https://github.com/cado-security/masked-ai',
+    download_url='https://github.com/cado-security/masked-ai/archive/refs/heads/main.zip',
+    py_modules=['masked-ai'],
+    install_requires=['nltk'],
+    packages=find_packages()
+)
